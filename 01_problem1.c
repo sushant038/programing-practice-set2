33c3b5e705f82fb2387779aa0461db21788bfc0b
Sushant Gawali <sushantgawali38@gmail.com>
2026-01-16 02:44:12 +0530

    Add files via upload

---
+++ 01_problem1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    // which of the following is valid in c ?
+    int a=1;
+    int b=a;
+    int v=3*3;
+  //  char dt='21 dec 2020';// wrong
+    return 0;
+
+}
---
+++ 05_problem5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    float a =3.0+1;
+    printf("The value of a is %f",a);
+    return 0;
+
+}
